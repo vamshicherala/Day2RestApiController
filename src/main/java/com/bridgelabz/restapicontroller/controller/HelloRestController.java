b0ee0583e24dee67eb4d9c235630482e2a58007d
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-27 02:47:51 +0530

    Added:Rest  api for post with json as request body

--- src/main/java/com/bridgelabz/restapicontroller/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicontroller/controller/HelloRestController.java
@@ -2,14 +2,19 @@ package com.bridgelabz.restapicontroller.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.restapicontroller.controller.User;
+
 @RestController
 @RequestMapping("/hello")
-public class HelloRestController{	
+public class HelloRestController 
+{	
 	@RequestMapping(value = {"","/","/home"})
 	public String hello() {
 		return "Hello from Bridgelabz";
@@ -22,5 +27,9 @@ public class HelloRestController{
 	@GetMapping("/get/{name}")
 	public String helloOne(@PathVariable String name) {
 		return "Hello" +" " + name;
+	}
+	@PostMapping("/post")
+	public String hello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() +  user.getLastName();
 	}
 }
