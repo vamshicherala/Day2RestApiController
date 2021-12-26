c646e2aec7cd2032df41bf43ad7afcac1f6c7d4e
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-27 02:42:23 +0530

    Added:Path variable added

--- src/main/java/com/bridgelabz/restapicontroller/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicontroller/controller/HelloRestController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.restapicontroller.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-/*
- * @RestController:to create the rest controller class.
- * @RequestMapping:describe the URL.
- * @RequestParam:to pass the value as parameter.
- */
-public class HelloRestController {
-	@RequestMapping(value = { "", "/", "/home" })
-	public String sayHello() {
-		return "Hello from BridgeLabz";
+public class HelloRestController{	
+	@RequestMapping(value = {"","/","/home"})
+	public String hello() {
+		return "Hello from Bridgelabz";
 	}
-	 @RequestMapping(value={"/query"}, method=RequestMethod.GET)
-	    public String sayHello(@RequestParam(value="name") String name) {
-	        return "Hello" + name + "From Bridgelabz";
-	    }
+	
+	@RequestMapping(value="/query",method = RequestMethod.GET)
+	public String hello(@RequestParam (value = "name") String name) {
+		return "Hello" +" " + name;
+	}
+	@GetMapping("/get/{name}")
+	public String helloOne(@PathVariable String name) {
+		return "Hello" +" " + name;
+	}
 }
